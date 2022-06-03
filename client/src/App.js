034433f0c049e796e6ac2b223388ef34abda5398
Lyssie <lyssiewright.15@gmail.com>
2022-06-03 18:37:55 -0500

    new routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,26 @@ import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch("/hello")
-      .then((r) => r.json())
-      .then((data) => setCount(data.count));
-  }, []);
 
   return (
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
+        <Route path="/">
+            <h1>Hello</h1>
           </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
+          <Route path="/skills">
+            <h1>Skills</h1>
+          </Route>
+          <Route path="/projects">
+            <h1>Projects</h1>
+          </Route>
+          <Route path="/resume">
+            <h1>Resume</h1>
+          </Route>
+          <Route path="/contact">
+            <h1>Contact me</h1>
           </Route>
         </Switch>
       </div>
